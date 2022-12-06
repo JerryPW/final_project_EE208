a5f18f770b605d8866e487305373a5844aa298fc
JerryPW <103237744+JerryPW@users.noreply.github.com>
2022-12-06 22:18:30 +0800

    finish the time order

--- app.py
+++ app.py
@@ -35,7 +35,20 @@ def parseCommand(command):
     command_dict['contents'] = ' '.join(jieba.cut(command))
     return command_dict
 
-def runs(searcher, analyzer,command):
+def timeCompare(time1, time2): # 1 -> time1 >= time2; 0 -> times2 > time1
+    time1 = time1.split('-')
+    time2 = time2.split('-')
+    for i in range(len(time1)):
+        if int(time1[i]) > int(time2[i]):
+            return 1
+        elif int(time1[i]) < int(time2[i]):
+            return 0
+        else:
+            continue
+    return 1
+
+
+def runs(searcher, analyzer,command, mode):
     global title,url,tt
     global tll,uu,con,time
     tll = []
@@ -58,7 +71,10 @@ def runs(searcher, analyzer,command):
         print("%s total matching documents." % len(scoreDocs))
         query = QueryParser('contents', analyzer).parse(command)
 
-
+        titlst = []
+        urlst = []
+        timlst = []
+        contentlist = []
         for scoreDoc in scoreDocs:
             doc = searcher.doc(scoreDoc.doc)
             title = doc.get('title')
@@ -68,9 +84,30 @@ def runs(searcher, analyzer,command):
             soup =  BeautifulSoup(contents,features="html.parser")
             contents = ''.join(soup.findAll(text=True))
             contents = ' '.join(jieba.cut(contents))
+            titlst.append(title)
+            urlst.append(url)
+            timlst.append(tt)
+            contentlist.append(contents)
+
+        if mode == "time":
+            for i in range(len(urlst)):
+                for j in range(len(urlst)-1-i):
+                    if timeCompare(timlst[j], timlst[j+1]) == 0:
+                        titlst[j], titlst[j+1] = titlst[j+1], titlst[j]
+                        timlst[j], timlst[j+1] = timlst[j+1], timlst[j]
+                        urlst[j], urlst[j+1] = urlst[j+1], urlst[j]
+                        contentlist[j], contentlist[j+1] = contentlist[j+1], contentlist[j]
+
+
+        # for scoreDoc in scoreDocs:
+        for i in range(len(urlst)):
+            title = titlst[i]
+            url = urlst[i]
+            tt = timlst[i]
+            contents = contentlist[i]
  
 
-            formatter = SimpleHTMLFormatter('<point>','</point>')           #           1
+            formatter = SimpleHTMLFormatter('"','"')           #           1
             highlighter =  Highlighter(formatter,QueryScorer(query))   
             highlighter.setTextFragmenter(SimpleFragmenter(25))
             tmp = analyzer.tokenStream("contents",contents)                #           1
@@ -82,15 +119,15 @@ def runs(searcher, analyzer,command):
                 continue
             tll.append(title)
             uu.append(url)
-            time.append(tt)
-
-                     
+            time.append(tt)            
         break
+
 app = Flask(__name__)
 @app.route('/', methods=['POST', 'GET'])
 def bio_data_form():
     if request.method == "POST":
         keyword = request.form['keyword']
+        mode = request.form['mode']
         return redirect(url_for('result', keyword=keyword))
     return render_template("bio_form.html")
 
@@ -98,7 +135,7 @@ def bio_data_form():
 @app.route('/result', methods=['GET'])
 def result():
     
-    global tll,uu,con,time
+    global tll,uu,con,time, last_search
     STORE_DIR = "163_index"
 
     vm.attachCurrentThread()   #一旦线程被附加到JVM上，这个函数会返回一个属于当前线程的JNIEnv指针
@@ -108,11 +145,16 @@ def result():
     # analyzer = StandardAnalyzer()
     analyzer = SimpleAnalyzer()
 
-    
     keyword = request.args.get('keyword')
+    if keyword == '':
+        keyword = last_search
+    elif keyword != '':
+        last_search = keyword
     keyword = ' '.join(jieba.cut(keyword))
 
-    runs(searcher, analyzer,keyword)
+    mode = request.args.get('mode')
+
+    runs(searcher, analyzer,keyword, mode)
     length = min(len(tll),len(uu))
     length = min(length,len(con))
     length = min(length,len(time))
@@ -124,9 +166,10 @@ def result():
 
 
 if __name__ == '__main__':
+    last_search = ''
 
     vm = lucene.initVM()
 
-    app.run(debug=True,port = 8080)
+    app.run(debug=True,port = 8081)
     
     
